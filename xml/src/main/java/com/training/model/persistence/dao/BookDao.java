9c9a0849cfb511f2e5fee42cc1c20d7912b0ae56
Digvijay Sunil <93257010+shaugrin@users.noreply.github.com>
2025-01-24 16:29:13 +0530

    Add files via upload

---
+++ xml/src/main/java/com/training/model/persistence/dao/BookDao.java
@@ -0,0 +1,13 @@
+package com.training.model.persistence.dao;
+
+import com.training.model.persistence.entities.Book;
+
+import java.util.List;
+
+public interface BookDao {
+    List<Book> getAllBooks();
+    Book addBook(Book book);
+    void deleteBook(int id);
+    void updateBook(int id, Book book);
+    Book getBookById(int id);
+}
