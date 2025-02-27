9ffa7da513c7e3fb7a3e819f6d6e49d2927f93c2
Digvijay Sunil <93257010+shaugrin@users.noreply.github.com>
2025-02-27 15:43:11 +0530

    Add files via upload

---
+++ aws/day3/ex1-s3-sb/src/main/java/com/s3/examples/S3ExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.s3.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ExamplesApplication.class, args);
+	}
+}
---
+++ aws/day3/ex1-s3-sb/src/main/java/com/s3/examples/service/FileStore.java
@@ -0,0 +1,61 @@
+package com.s3.examples.service;
+
+import java.io.File;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+
+@Service
+public class FileStore {
+
+	Logger logger = LogManager.getLogger(this.getClass().getName());
+
+	@Autowired
+	private AmazonS3 amazonS3;
+
+
+	public String createBucket(String bucketName) {
+		logger.info("Inside method createBucket");
+		try {
+			if (bucketAlreadyExists(bucketName)) {
+				logger.error("bucket already exist");
+			}
+			amazonS3.createBucket(bucketName);
+		} catch (AmazonS3Exception s3Exception) {
+			logger.error("Unable to create bucket :" + s3Exception.getMessage());
+		}
+		return "Bucket created with name:" + bucketName;
+	}
+
+	private boolean bucketAlreadyExists(String bucketName) {
+		logger.info("Inside method bucketAlreadyExists");
+		return amazonS3.doesBucketExistV2(bucketName);
+	}
+
+	public void uploadFiletoBucket(MultipartFile multiPart,String bucketName) throws Exception {
+		logger.info("Inside method upload");
+		File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + multiPart.getOriginalFilename());
+		multiPart.transferTo(convFile);
+		try {
+			amazonS3.putObject(bucketName, convFile.getName(), convFile);
+			
+		} catch (AmazonS3Exception s3Exception) {
+			logger.error("Unable to upload file :" + s3Exception.getMessage());
+		}
+	}
+
+	public void deleteBucket(String bucketName) {
+		amazonS3.deleteBucket(bucketName);
+	}
+
+	public void deletFile(String bucketName, String fileName) {
+		amazonS3.deleteObject(bucketName, fileName);
+	}
+
+}
